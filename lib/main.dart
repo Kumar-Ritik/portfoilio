6ce9fa846af7fe158235d426121a75ce4d56fba4
Ritik <ara.kumarritik>
2022-10-06 22:01:30 +0530

    UI done Trying experiment

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,19 @@ import 'package:portfolio_app/pages/loginpage.dart';
 import 'firebase_options.dart';
 import 'pages/homepage.dart';
 
-// sbse phele ye package import kro
+// sbse phele ye package import kro DefaultFirebaseOptions.currentPlatform,
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyC2I5yN-bUiXl8E9Ltc6amwaXoltqDrAe4",
+    //authDomain: "ritik-firebase.firebaseapp.com",
+    projectId: "ritik-firebase",
+    storageBucket: "ritik-firebase.appspot.com",
+    messagingSenderId: "167904333192",
+    appId: "1:167904333192:web:4b90ef93d0d22305ccbe3d",
+    //measurementId: "G-1DL3GR5RLQ"
+  ));
   runApp(MyApp());
 }
 
@@ -23,6 +30,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //home: Homepage(),
+      debugShowCheckedModeBanner: false,
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => Homepage(),
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,10 +1,31 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+//import 'package:get/get_core/src/get_main.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+import 'job.dart';
+import 'login_text.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //const LoginPage({Key? key}) : super(key: key);
+  final controller = Get.put(LoginController());
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,74 +38,115 @@ class LoginPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Image.asset(
-              "assets/images/job.png",
-              fit: BoxFit.cover,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Enter Username",
-                labelText: "Username",
+      body: SafeArea(
+        child: Container(
+          //height: ,
+          //width: MediaQuery.of(context).size.width,
+          //height: 100,
+          padding: const EdgeInsets.symmetric(horizontal: 100),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Container(),
+                flex: 2,
               ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Enter Password",
-                labelText: "Password",
+              Image.asset(
+                "assets/images/job.png",
+                //fit: BoxFit.cover,
+                height: 64,
               ),
-            ),
-            FloatingActionButton.extended(
-              onPressed: () {
-                GoogleSignIn().signIn();
-              },
-              icon: Image.asset(
-                "images/google.png",
-                fit: BoxFit.cover,
-                height: 32,
-                width: 32,
-                //colorBlendMode: Colors.white,
+              TextFieldInput(
+                hintText: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailController,
               ),
-              label: Text("Sign in with Google"),
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.blue,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Image.asset(
-              "images/hired.png",
-              fit: BoxFit.cover,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Enter Username",
-                labelText: "Username",
+              const SizedBox(
+                height: 24,
               ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Enter Password",
-                labelText: "Password",
+              TextFieldInput(
+                hintText: 'Enter your password',
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPass: true,
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 24,
+              ),
+              Builder(builder: (context) {
+                return FloatingActionButton.extended(
+                  onPressed: () {
+                    GoogleSignIn().signIn();
+                  },
+                  icon: Image.asset(
+                    "images/google.png",
+                    fit: BoxFit.cover,
+                    height: 32,
+                    width: 32,
+                    //colorBlendMode: Colors.white,
+                  ),
+                  label: Text("Sign in with Google"),
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.blue,
+                );
+              }),
+              SizedBox(
+                height: 10,
+              ),
+              Image.asset(
+                "images/hired.png",
+                //fit: BoxFit.cover,
+                height: 64,
+              ),
+              TextFieldInput(
+                hintText: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                hintText: 'Enter your password',
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPass: true,
+              ),
+              // const SizedBox(
+              //   height: 24,
+              // ),
+              InkWell(
+                child: Container(
+                  child: !_isLoading
+                      ? const Text(
+                          'Log in',
+                        )
+                      : const CircularProgressIndicator(
+                          color: Color.fromARGB(198, 18, 190, 116),
+                        ),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
+                    ),
+                    color: Color.fromARGB(236, 163, 0, 0),
+                  ),
+                ),
+                //onTap: loginUser,
+              ),
+              // const SizedBox(
+              //   height: 12,
+              // ),
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+            ],
+          ),
         ),
       ),
-      // Column(
-      //   //mainAxisAlignment: MainAxisAlignment.center,
-      //   //crossAxisAlignment: CrossAxisAlignment.start,
-      //   children: [
-      //     Image.asset(
-      //       "images/hired.png",
-      //       fit: BoxFit.cover,
-      //     ),
-      //   ],
-      // ),
     );
   }
 }
